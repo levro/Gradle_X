810ca0165b3e39de7bb17b651327ff7d737f2b46
bios <bios@ukr.net>
2018-12-23 21:25:35 +0200

    xls tests refactoring

--- test_module/src/test/java/FileOpsTestCase.java
+++ test_module/src/test/java/FileOpsTestCase.java
@@ -2,17 +2,35 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
+import java.io.File;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class FileOpsTestCase {
-    private static final String XLS_FILE = "test_file.xls";
+    private static final String READ_XLS_FILE = "resources/read_xls_test.xls";
+    private static final String XLS_FILE = "src/test/temp_files/test_file.xls";
+    private static final String[] ROW_1 = {"One", "Two", "Three"};
+    private static final String[] ROW_2 = {"1.0", "2.0", "3.0"};
     private final static Logger log = LogManager.getLogger( FileOpsTestCase.class );
 
     @Test
+    public void testReadFromXls() {
+        log.info( "TEST_06: testReadFromXls" );
+        List<Object[]> objects = FileOperations.readXLS(READ_XLS_FILE);
+        int recordsAmount = ROW_1.length;
+        for ( int i = 0; i < recordsAmount; i++ ) {
+            assertEquals( ROW_1[i], objects.get( 0 )[i] );
+            assertEquals( ROW_2[i], objects.get( 1 )[i] );
+        }
+    }
+
+    @Test
     public void testAppendToXls() {
-        log.error( "TEST_03: testAppendToXls" );
+        log.info( "TEST_03: testAppendToXls" );
+        if ( !new File(XLS_FILE).isFile() ) {
+            FileOperations.writeXLS(new String[]{}, XLS_FILE);
+        }
         String[] xlsRecords = new String[]{ "ABCDEFGH" };
         FileOperations.appendXLS( xlsRecords, XLS_FILE );
         List<Object[]> objects = FileOperations.readXLS( XLS_FILE );
@@ -21,13 +39,13 @@ public class FileOpsTestCase {
 
     @Test
     public void testWriteToXls() {
-        log.error( "TEST_04: testWriteToXls" );
+        log.info( "TEST_04: testWriteToXls" );
         String[] xlsRecords = { "Test1", "100", "" };
         FileOperations.writeXLS( xlsRecords, XLS_FILE );
         List<Object[]> objects = FileOperations.readXLS( XLS_FILE );
         int recordsAmount = xlsRecords.length;
         for ( int i = 0; i < recordsAmount; i++ ) {
-            assertEquals( xlsRecords[recordsAmount - 1], objects.get( recordsAmount - 1 )[0] );
+            assertEquals( xlsRecords[i], objects.get( i )[0] );
         }
     }
 }
